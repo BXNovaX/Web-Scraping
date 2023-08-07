5211242f97d9b6ed393a6c423fdb42c5c79a96c4
Hosein Moghaddam <mhmk.nova@gmail.com>
2023-08-07 15:45:38 +0330

    Hi There!

--- main.py
+++ main.py
@@ -1,36 +1,105 @@
 from bs4 import BeautifulSoup
-import httpx
-import asyncio
+from persiantools.jdatetime import JalaliDate
+from dotenv import load_dotenv
+import smtplib, ssl, re, asyncio, httpx, os
+import time
+
+load_dotenv()
 
 
 """
 TODO Clean this mess!
 
-typography__StyledDynamicTypographyComponent-t787b7-0 ibfopD BrowseArticleListItemDesktop___StyledTypography-sc-1szqe4e-0 hYItiO
-typography__StyledDynamicTypographyComponent-t787b7-0 ibfopD BrowseArticleListItemDesktop___StyledTypography-sc-1szqe4e-0 hYItiO
-
-box__BoxBase-sc-1ww1anb-0 kXWFxm BrowseArticleListItemDesktop__ContentBox-sc-1szqe4e-5 gpOrSi
-box__BoxBase-sc-1ww1anb-0 kXWFxm BrowseArticleListItemDesktop__ContentBox-sc-1szqe4e-5 gpOrSi
+ou can use regular expressions to replace or remove the parts that are not integers. For example, ; new_string = re.sub (r'\D+', '', old_string)
 """
+
+
+PORT = 587
+SMTP_SERVER = "smtp.gmail.com"
+USERNAME = "mhmk.nova"
+PASSWORD = os.getenv("PASSWORD")
+print(USERNAME, PASSWORD)
+SENDER_EMAIL = f"{USERNAME}@gmail.com"
+CONTEXT = ssl.create_default_context()
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Cache-Control": "max-age=0",
+}
+
+
+def send_mail(receiver_mail, message):
+    with smtplib.SMTP(SMTP_SERVER, PORT) as server:
+        server.starttls(context=CONTEXT)
+        server.login(SENDER_EMAIL, PASSWORD)
+        server.sendmail(SENDER_EMAIL, receiver_mail, message)
 
 
 async def main():
     async with httpx.AsyncClient() as client:
-        r = await client.get("https://bama.ir/car?transmission=automatic")
-    soup = BeautifulSoup(r, "lxml")
-    boxes = soup.find_all(
-        "div",
-        class_="bama-adlist-container",
-    )
-    with open("output.txt", "w") as tags_txt:
-        for box in boxes:
-            tags_txt.write(
-                f"""
-{box.a.find("div", class_="bama-ad__price-row").find("div", class_="bama-ad__price-holder").span.text}
-\n\n
+        response = await client.get(
+            url="https://bama.ir/car?brand=quick,atfullplus,manualrplus(at)&priced=1&sort=4",
+            headers=HEADERS,
+            timeout=5.0,
+        )
+    if response.status_code == 200:
+        soup = BeautifulSoup(response, "lxml")
+        with open("index.html", "w") as index:
+            index.write(soup.prettify())
+        boxes = soup.find(
+            "div",
+            class_="bama-adlist-container",
+        )
+        with open("output.txt", "w") as output:
+            for box in boxes.find_all("div", class_="bama-ad-holder"):
+                model = box.a.find("div", class_="bama-ad__title-row").p.text.strip()
+                year = box.a.find("div", class_="bama-ad__detail-row").span.text.strip()
+                distance = re.sub(
+                    r"\D+",
+                    "",
+                    box.a.find("div", class_="bama-ad__detail-row")
+                    .find_all("span")[1]
+                    .text.strip()
+                    .replace(",", "_"),
+                )
+                detail = (
+                    box.a.find("div", class_="bama-ad__detail-row")
+                    .find_all("span")[2]
+                    .text.strip()
+                )
+                price = (
+                    box.a.find("div", class_="bama-ad__price-row")
+                    .find("div", "bama-ad__price-holder")
+                    .span.text.strip()
+                ).replace(",", "")
+                link = box.a["href"]
+                output.write(
+                    f"""
+Model: {model}
+Manufacture Year: {year}
+Distance Passed: {0 if distance == "" else distance}
+Detail: {detail}
+Price: {price}
+link: {f"https://bama.ir{link}"}
 """
-            )
+                )
+        return 0
+    else:
+        print(response)
+        return 1
 
 
 if __name__ == "__main__":
+    # send_mail("mhmk.nova@pm.me", "Hello World!")
+    start_time = time.time()
     asyncio.run(main())
+    print("--- %s seconds ---" % (time.time() - start_time))
